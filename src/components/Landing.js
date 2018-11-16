2bd40d7a34e2f3da0ef362978f41eff8ccf4c758
Adrienne King <AdrienneNKing@gmail.com>
2018-11-16 13:42:49 -0800

    Bloc Jams React Routing Checkpoint Submission

---
+++ src/components/Landing.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+
+const Landing = () => {
+  return (
+    <section className="landing">
+    Landing page will go here
+    </section>
+  );
+}
+
+export default Landing;
