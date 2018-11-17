15c28eb56514b19932b0d6545ca6b2474065f61d
Adrienne King <AdrienneNKing@gmail.com>
2018-11-16 16:59:41 -0800

    Bloc Jams Components: Library Checkpoint Submission

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -10,9 +10,9 @@ const Landing = () => {
         <section className="selling-points">
             <div className="point">
               <h2 className="point-title">Choose your music</h2>
-              <p classname="point-description">The world is full of music; why should you have to listen to music that someone else chose?</p>
+              <p className="point-description">The world is full of music; why should you have to listen to music that someone else chose?</p>
             </div>
-            <div classname="point">
+            <div className="point">
               <h2 className="point-title">Unlimited, streaming, ad-free</h2>
               <p className="point-description">No arbitrary limits. No distractions.</p>
             </div>
