0604cb362522882219761fe5b5489f684513aa04
Adrienne <adriennenking@gmail.com>
2018-12-18 15:04:49 -0800

    Bloc Jams React Player Bar Buttons Assignment

--- src/components/Album.js
+++ src/components/Album.js
@@ -55,6 +55,14 @@ handlePrevClick() {
   this.play();
 }
 
+handleNextClick() {
+  const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
+  const newIndex = Math.max(0, currentIndex + 1);
+  const newSong = this.state.album.songs[newIndex];
+  this.setSong(newSong);
+  this.play();
+}
+
   renderNumber(song, songNumber) {
     var isSameSong = this.state.currentSong ===song;
     if (this.state.isHovered && this.state.isHovered.audioSrc === song.audioSrc) {
@@ -117,6 +125,7 @@ handlePrevClick() {
           currentSong={this.state.currentSong}
           handleSongClick={() => this.handleSongClick(this.state.currentSong)}
           handlePrevClick={() => this.handlePrevClick()}
+          handleNextClick={() => this.handleNextClick()}
         />
 
       </section>
